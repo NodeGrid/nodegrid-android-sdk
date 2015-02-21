39b9c70f610dffee15cc5aa23f8b617bdddc7883
Kavimal Wijewardana <kavi707@gmail.com>
2015-02-21 23:44:48 +0530

    Add initial project structure

---
+++ src/com/nodegrid/android/sdk/CommonUtils.java
@@ -0,0 +1,10 @@
+package com.nodegrid.android.sdk;
+
+/**
+ * Created by kavi707 on 2/21/15.
+ * @author Kavimal Wijewardana <kavi707@gmail.com>
+ */
+public class CommonUtils {
+
+    public static final String NODEGRID_SERVER_URL = "";
+}
