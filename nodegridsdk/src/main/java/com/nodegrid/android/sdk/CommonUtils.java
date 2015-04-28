c6a89054a4c566ff728d0f1eeb85ae640c8da933
Kavimal Wijewardana <kavi707@gmail.com>
2015-04-28 10:13:40 +0530

    Start to implement 'app' end-points from SDK

--- nodegridsdk/src/main/java/com/nodegrid/android/sdk/CommonUtils.java
+++ nodegridsdk/src/main/java/com/nodegrid/android/sdk/CommonUtils.java
@@ -6,7 +6,7 @@ package com.nodegrid.android.sdk;
  * @author Kavimal Wijewardana <kavi707@gmail.com>
  */
 public class CommonUtils {
-    public static final String NODEGRID_SERVER_URL = "http://172.26.203.24:3000";
+    public static final String NODEGRID_SERVER_URL = "http://172.26.203.12:3000";
     public static final String HTTP_GET = "GET";
     public static final String HTTP_POST = "POST";
     public static final String HTTP_PUT = "PUT";
--- nodegridsdk/src/main/java/com/nodegrid/android/sdk/services/connections/AppApiCalls.java
+++ nodegridsdk/src/main/java/com/nodegrid/android/sdk/services/connections/AppApiCalls.java
@@ -1,9 +1,27 @@
 package com.nodegrid.android.sdk.services.connections;
 
+import com.nodegrid.android.sdk.CommonUtils;
+
+import java.util.Map;
+
 /**
  * Created by kwijewardana on 4/8/15.
  *
  * @author Kavimal Wijewardana <kavi707@gmail.com>
  */
 public class AppApiCalls extends ApiConnector {
+
+    public String readAllCollectionObjects (String collectionName, Map<String, String> headerParams) {
+        String resultResponse;
+        resultResponse = sendHttpRequest(CommonUtils.NODEGRID_SERVER_URL + "/app/" + collectionName,
+                CommonUtils.HTTP_GET, headerParams);
+        return resultResponse;
+    }
+
+    public String readCollectionObjectFromId (String collectionName, String objectId, Map<String, String> headerParams) {
+        String resultResponse;
+        resultResponse = sendHttpRequest(CommonUtils.NODEGRID_SERVER_URL + "/app/" + collectionName +
+                        "/" + objectId, CommonUtils.HTTP_GET, headerParams);
+        return resultResponse;
+    }
 }
